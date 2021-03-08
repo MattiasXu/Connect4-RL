897e24204fa8df80acff230d8aed55c37866e047
Mattias Xu <mattias_xu@hotmail.com>
2021-03-08 14:02:50 +0100

    Added Manual Actor

--- actors.py
+++ actors.py
@@ -9,6 +9,19 @@ class RandomActor():
         actions = np.argwhere(obs['action_mask']).reshape(-1)
         return np.random.choice(actions)
 
+class ManualActor():
+    def __init__(self, env):
+        self.env = env
+
+    def act(self, obs):
+        actions = np.argwhere(obs['action_mask']).reshape(-1)
+        if actions[0] == 7:
+            return 7
+        else:
+            self.env.render()
+            action = int(input("Choose column... ")) - 1 
+            print(action)
+        return action
 
 class FCPolicy(nn.Module):
     """Fully Connected Model"""
