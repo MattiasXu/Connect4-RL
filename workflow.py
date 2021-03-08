f63a9a44e4d6f2786a7737fcb20d7eb26b160b48
Mattias Xu <mattias_xu@hotmail.com>
2021-03-08 13:38:10 +0100

    Agent renamed to actor

--- workflow.py
+++ workflow.py
@@ -10,7 +10,7 @@ import actors
 actor1 = actors.RandomActor()
 actor2 = actors.RandomActor()
 
-agents = [actor1, actor2]
+actors = [actor1, actor2]
 env = gym.make('Connect4Env-v0')
 obses = env.reset()  # dict: {0: obs_player_1, 1: obs_player_2}
 
@@ -18,18 +18,10 @@ obses = env.reset()  # dict: {0: obs_player_1, 1: obs_player_2}
 game_over = False
 while not game_over:
     action_dict = {}
-    for agent_id, agent in enumerate(agents):
-        action = agent.act(obses[agent_id])
-        action_dict[agent_id] = action
+    for actor_id, actor in enumerate(actors):
+        action = actor.act(obses[actor_id])
+        action_dict[actor_id] = action
     
     obses, rewards, game_over, info = env.step(action_dict)
     env.render()
     input("Press Enter to continue...")
-
-
-
-
-"""
-TODO: Fix one-hot encoding
-TODO: Integrate PyTorch
-"""
